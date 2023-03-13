4a4364e2d75fac59bf12418e6c1e919c004a70ce
Joshan Dasireddy <joshan.dasireddy@argano.com>
2023-03-13 18:10:03 +0530

    update not null

--- src/main/java/com/javatechie/crud/entity/Order.java
+++ src/main/java/com/javatechie/crud/entity/Order.java
@@ -14,6 +14,7 @@ import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
 import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.lang.NonNull;
 
 @Data
 @AllArgsConstructor
@@ -29,5 +30,6 @@ public class Order {
     private double price;
     @CreationTimestamp
     @Temporal(TemporalType.TIMESTAMP)
+    @NonNull
     private Date date;
 }
